4f2afe1d3634604bf168cc6f40e5ce6b47aaa4c1
huuga <huuga@inbox.ru>
2018-01-02 22:51:30 +0900

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,9 @@
 package ru.job4j;
 
+/**
+* Вывод приветственной строки
+* @param arg - arg.
+*/
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculating
+*
+* @author Eugene Vasiliev (huuga@inbox.ru)
+* @version $Id$
+* @since 01.01.2017
+*/
+package ru.job4j;
