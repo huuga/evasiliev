25c36cd0dc7bf0e5fa540c87a54c7b88ed5bad00
huuga <huuga@inbox.ru>
2018-01-09 12:33:34 +0900

    1.4 JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,11 +1,27 @@
 package ru.job4j;
 
 /**
-* Вывод приветственной строки
-* @param arg - arg.
-*/
+ * Calculate
+ *
+ * @author Eugene Vasilev (huuga@inbox.ru)
+ * @version $Id$
+ * @since 01.01.2018
+ */
 public class Calculate {
+	/**
+	 * Main
+	 * @param args - args
+	 */	
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 	}
+	
+	/**
+	 * Method echo
+	 * @param name Your name
+	 * @return Echo plus your name
+	 */
+ 	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}	
 }
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for testing
+*
+* @author Eugene Vasiliev (huuga@inbox.ru)
+* @version $Id$
+* @since 09.01.2018
+*/
+package ru.job4j;
